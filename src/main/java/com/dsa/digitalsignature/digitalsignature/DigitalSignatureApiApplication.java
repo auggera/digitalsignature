2d709680f2dfd750022b4c1cfff5fc0a6a4fc745
Augustin Gerasym <augustingerasym@gmail.com>
2025-02-11 14:38:16 +0100

    Initial commit

---
+++ src/main/java/com/dsa/digitalsignature/digitalsignature/DigitalSignatureApiApplication.java
@@ -0,0 +1,13 @@
+package com.dsa.digitalsignature.digitalsignature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalSignatureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalSignatureApiApplication.class, args);
+	}
+
+}
